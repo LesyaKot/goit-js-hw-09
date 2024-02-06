a5cb1b698d40857c2d883d15b68c5057e7ac1cb0
LesyaKot <145243873+LesyaKot@users.noreply.github.com>
2024-02-06 14:12:55 +0000

    Deploying to gh-pages from @ LesyaKot/goit-js-hw-09@3c0355b6166b2c6114b2171176651528db0a8f0e 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-3c4e11fa.js";console.log("Form");
+import"./assets/styles-3c4e11fa.js";import{S as o}from"./assets/vendor-870f0eb5.js";new o(".form a",{});console.log(o);
 //# sourceMappingURL=commonHelpers2.js.map
