3c0355b6166b2c6114b2171176651528db0a8f0e
LesyaKot <lesyakot83@gmail.com>
2024-02-06 19:12:37 +0500

    upd

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,7 @@
-console.log("Form");
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const lightbox = new SimpleLightbox('.form a', { /* options */ });
+
+console.log(SimpleLightbox)
