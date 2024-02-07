47645fe4aae69443e83b1af77dcc618b82167e9f
LesyaKot <lesyakot83@gmail.com>
2024-02-07 12:17:01 +0500

    upd

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,7 +1,30 @@
-import SimpleLightbox from "simplelightbox";
+const storageKey = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
 
-import "simplelightbox/dist/simple-lightbox.min.css";
+function readFormData(form) {
+  const email = form.email.value;
+  const message = form.message.value;
+  return { email, message };
+}
 
-const lightbox = new SimpleLightbox('.form a', { /* options */ });
+form.addEventListener('input', event => {
+  event.preventDefault();
+  const data = readFormData(event.currentTarget);
+  const jsonData = JSON.stringify(data);
+  localStorage.setItem(storageKey, jsonData);
+});
 
-console.log(SimpleLightbox)
+const rawData = localStorage.getItem(storageKey);
+if (rawData) {
+  const data = JSON.parse(rawData);
+  form.email.value = data.email;
+  form.message.value = data.message;
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(readFormData(form));
+
+  localStorage.clear();
+  form.reset();
+});
